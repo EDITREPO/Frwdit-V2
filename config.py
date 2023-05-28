836d50395e060b5e0d1cc14de6bb5f8c0574340b
EDITREPO <105662364+EDITREPO@users.noreply.github.com>
2023-05-28 23:19:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,13 +7,13 @@ import logging
 
 class Config:
     
-    API_ID = int(os.environ.get("API_ID", 12345))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+    API_ID = int(os.environ.get("8595805"))
+    API_HASH = os.environ.get("ca430a2687f7fc25c60e8c83854d29ba")
+    BOT_TOKEN = os.environ.get("5157509263:AAHL3qOkw0CS_rEnPvnIFA4hu1_Jikw8ELA") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "bot") 
     CAPTION = os.environ.get("CAPTION", "")
     FILTER_TYPE = os.environ.get("FILTER_TYPE", "document")
-    OWNER_ID = os.environ.get("OWNER_ID", 12345)
-    SESSION = os.environ.get("SESSION")
+    OWNER_ID = os.environ.get("1145137954")
+    SESSION = os.environ.get("BQCDKV0AVdo4s_A1vrPTk1BXCOKfMoKJYVLkhvhAbhClmlV3o0y_kQqipotUNeh8g_gUr4zfuJhu4ohM9tnSO-1XRftZUpYqiGGGCy1U5ovmTUJrjA6dqziLwk85FJSozA65Voso0_eva-7JJv0tZpaM-ENH4n1yfMRsOhpUOXtAtU56J5LhvM6CuHRxEAMJDC-BeV5XkwgsPt1ihooZJnznfO63c9NuOA7c_hozigtrpSusWKTKBxWHAULta-kYeIhv5b6o46Z9aUZeB-A5zmMXbrxsVUu25FWE_5o_7bitsJ0LcwHtl8WSo4boqLMzncXCBOAZqyQJDvAq3ahggb-SXZP0QAAAAABEQWsiAA")
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
